2fb82dd8055bc3bbb2dbacbcd259a645684a7d6a
rishivajre <rishikeshvajre7@gmail.com>
2025-03-31 19:05:05 +0530

    coding practice

--- codingPrctc/reverseStringSB.java
+++ codingPrctc/reverseStringSB.java
@@ -22,5 +22,16 @@ public class reverseStringSB {
             sb.setCharAt(back, frontChar);
         }
         System.out.println(sb); // Output: Hello
+
+        // OR || Alternatively, you can use the following method:
+        StringBuilder sb2 = new StringBuilder("World");
+        sb2.reverse();
+        System.out.println(sb2); // Output: dlroW
+
+        // OR || Alternatively, you can use the following method:
+        String str2 = "World 2";
+        String reversedstr2 = new StringBuilder(str2).reverse().toString();
+        System.out.println(reversedstr2); // Output: dlroW
+
     }
 }
