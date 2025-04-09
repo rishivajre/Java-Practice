14f475ce2710fd1ba3288da7d15ca1b4b5439242
rishivajre <rishikeshvajre7@gmail.com>
2025-04-09 14:26:03 +0530

    Split and Reverse words using StringBuffer/StringBuilder

--- PracticalCoding/REVERSE WORDS/SplitAndReverse.java
+++ PracticalCoding/REVERSE WORDS/SplitAndReverse.java
@@ -1,3 +1,19 @@
 public class SplitAndReverse {
-    p
+
+    public static String reverseString(String sentence) {
+        if (sentence == null) return null;
+
+        String[] words = sentence.trim().split("\\s+");
+        StringBuffer s = new StringBuffer();
+
+        for(int i=words.length-1; i>=0; i--) {
+            s.append(words[i]);
+            if (i>0) s.append(" ");
+        }
+        return s.toString();
+    }
+    public static void main(String[] args) {
+        String sentence = "Right is Wrong";
+        System.out.println(reverseString(sentence));
+    }
 }
