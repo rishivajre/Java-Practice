ad9ea27c28754f515bc6a134ec80655a16c8ad61
rishivajre <rishikeshvajre7@gmail.com>
2025-04-20 12:00:43 +0530

    Update test input in ZeroTwoPointer class to validate zero movement functionality

--- PracticalCoding/NUMBER Manipulations/Move Zeros/ZeroTwoPointer.java
+++ PracticalCoding/NUMBER Manipulations/Move Zeros/ZeroTwoPointer.java
@@ -7,13 +7,12 @@ public class ZeroTwoPointer {
         for (int i=0; i<arr.length; i++) {
             if (arr[i] != 0) arr[nonZeroPos++] = arr[i];
         }
-
         while (nonZeroPos < arr.length) arr[nonZeroPos++] = 0;
     }
 
 
     public static void main(String[] args) {
-        int[] arr = {1,0,2,3,4,9,8};
+        int[] arr = {0, 1, 0, 3, 12};
         moveZeroToEnd(arr);
         System.out.println(Arrays.toString(arr));
     }
