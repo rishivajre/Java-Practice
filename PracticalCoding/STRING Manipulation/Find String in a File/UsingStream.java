f9e8bbef5a06968c7c7bf906b13adaa8a5db767e
rishivajre <rishikeshvajre7@gmail.com>
2025-04-20 20:18:07 +0530

    Refactor UsingStream class for case-insensitive string search and improve exception handling

--- PracticalCoding/STRING Manipulation/Find String in a File/UsingStream.java
+++ PracticalCoding/STRING Manipulation/Find String in a File/UsingStream.java
@@ -1,21 +1,24 @@
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class UsingStream {
     public static boolean findStringInFile(String filePath, String target) throws IOException {
-        try (Stream<String> lines = new Files.lines(Paths.get(filePath))) {
-            return lines.anyMatch(line -> line.contains(target));
+        try(Stream<String> lines = Files.lines(Paths.get(filePath))) {
+            return lines.anyMatch(line -> line.toLowerCase().contains(target.toLowerCase()));          
+        } 
+    }
+
+    public static void main(String[] args) {
+        String filePath = "E:\\test.txt";
+        String target = "java";
+
+        try {
+            Boolean result = findStringInFile(filePath, target);
+            System.out.println(result);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static void main(String[] args) throws FileNotFoundException{
-        String filePath = "E:\\test.txt";
-        String target = "Java";
-
-        Boolean result = findStringInFile(filePath, target);
-        System.out.println(result);
     }
 }
