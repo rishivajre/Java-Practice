d484ee01d8f47d77f933fb710725419cacdd8d63
rishivajre <rishikeshvajre7@gmail.com>
2025-04-20 18:05:34 +0530

    Add UsingBufferReader class to find a specific string in a file

---
+++ PracticalCoding/STRING Manipulation/Find String in a File/UsingBufferReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class UsingBufferReader {    // Problem Statement: Find String in File
+    
+    public static boolean findStringInFile(String filePath, String target) throws FileNotFoundException{
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains(target)) return true;
+            }
+        }  catch (Exception e) {
+            // TODO: handle exception
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        String filePath = "E:\\test.txt";
+        String target = "Java";
+
+        Boolean result = findStringInFile(filePath, target);
+        System.out.println(result);
+    }
+}
+
+/**Ek file diya hai (text file),
+ *  aur hume check karna hai ki usme ek specific string (target) exist karti hai ya nahi. 
+ * If string milti hai, return true, else false. For example:
+ * 
+ * File content: "Hello\nWorld\nJava is fun"
+    Target: "Java"
+    Output: true
+ */
