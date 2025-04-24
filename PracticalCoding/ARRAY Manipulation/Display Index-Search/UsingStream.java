52dd261f463950fd535c5e1a68348e5f82c3b6b2
rishivajre <rishikeshvajre7@gmail.com>
2025-04-24 19:16:50 +0530

    Add UsingStream class to implement element index search using streams; include main method for user input and output of found indices

---
+++ PracticalCoding/ARRAY Manipulation/Display Index-Search/UsingStream.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class UsingStream {
+    public static void findIndicesWithStream(int[] arr, int searchElement) {
+        List<Integer> indices = IntStream.range(0, arr.length)
+                                .filter(n -> arr[n] == searchElement)
+                                .boxed()
+                                .collect(Collectors.toList());
+        if (indices.isEmpty()) {
+            System.out.println("Ele not found");
+        }else{
+            System.out.println("element found at indices: "+ indices);
+        }
+    }           
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of the array: ");
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements: ");
+        for (int i=0; i<size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Enter search element: ");
+        int searchElement = scanner.nextInt();
+        findIndicesWithStream(arr, searchElement);
+        scanner.close();
+    }
+}
