69db3e93ad297309ffa18443d249c094f0e1d3e0
Rishikesh Vajre <rishikeshvajre7@gmail.com>
2025-07-31 13:26:45 +0530

    Implement FactorialofNumber and UsingRecursion classes for calculating factorials using iteration and recursion

---
+++ 25 Qs SDET-Jatin/Factorial of A Number/FactorialofNumber.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+public class FactorialofNumber {
+
+    public static void main(String[] args) {
+        int num = 05;
+        BigInteger result = BigInteger.ONE;
+
+        for (int i=2; i<=num; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(result);
+    }
+
+}
---
+++ 25 Qs SDET-Jatin/Factorial of A Number/UsingRecursion.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class UsingRecursion {
+    public static BigInteger factorialByRecursion(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Factorial isn't defined for -ve Nums!!");
+        }
+        if (num == 0 || num == 1) {
+            return BigInteger.ONE;
+        }
+        return BigInteger.valueOf(num).multiply(factorialByRecursion(num - 1));
+    }
+
+    public static void main(String[] args) {
+        int num = 25;
+        System.out.println(factorialByRecursion(num));
+    }
+}
