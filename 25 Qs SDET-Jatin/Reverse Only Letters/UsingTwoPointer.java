9a5c0987422afd8acbcd71c3c6514cbbdaf80249
Rishikesh Vajre <rishikeshvajre7@gmail.com>
2025-08-03 09:16:02 +0530

    Refactor UsingTwoPointer class for improved readability; enhance code structure and maintainability.

--- 25 Qs SDET-Jatin/Reverse Only Letters/UsingTwoPointer.java
+++ 25 Qs SDET-Jatin/Reverse Only Letters/UsingTwoPointer.java
@@ -2,31 +2,42 @@ import java.util.Arrays;
 
 public class UsingTwoPointer { // Reverse Only Letters
     public static void main(String[] args) {
-    String input = "1abc3";
-    char[] inputArray = input.toCharArray();
-    
+        String input = "1abc3";
+        
+        // Convert the string to a char array for easy in-place modification
+        char[] inputArray = input.toCharArray();
 
-    int left = 0;
-    int right = inputArray.length-1;
+        // Two pointers from both ends of the array
+        int left = 0;
+        int right = inputArray.length - 1;
 
-    while(left<right) {
-        if (!Character.isLetter(inputArray[left])){
-            left++;
-        } else if (!Character.isLetter(inputArray[right])){
-            right--;
-        } else {
-            char temp = inputArray[left];
-            inputArray[left] = inputArray[right];
-            inputArray[right] = temp;
-            left++;
-            right--;
+        // Loop until both pointers meet
+        while (left < right) {
+
+            // If the left character is not a letter, skip it
+            if (!Character.isLetter(inputArray[left])) {
+                left++;
+            }
+            // If the right character is not a letter, skip it
+            else if (!Character.isLetter(inputArray[right])) {
+                right--;
+            }
+            // If both are letters, swap them
+            else {
+                char temp = inputArray[left];
+                inputArray[left] = inputArray[right];
+                inputArray[right] = temp;
+                left++;
+                right--;
+            }
         }
-    } 
-    System.out.println(Arrays.toString(inputArray));
-    System.out.println(new String(inputArray));
-    for (char c : inputArray){
-        System.out.print(c);
-    }
-    
+
+        // Printing the final char array using different ways
+        System.out.println("Array view: " + Arrays.toString(inputArray));
+        System.out.println("String view: " + new String(inputArray));
+        System.out.print("Char-by-char: ");
+        for (char c : inputArray) {
+            System.out.print(c);
+        }
     }
 }
